df09f320dc20a8d7e38eb34c5fb0d8f6d7b3ef9b
Yongsu Kang <dksktm15@naver.com>
2025-03-18 16:52:59 +0900

    AboutView 제거거

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,6 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import HomeView from "../views/HomeView.vue";
-import AboutView from "../views/AboutView.vue";
 import StadiumAddView from "../views/StadiumAddView.vue";
 import SignUpView from "../views/SignUpView.vue";
 
@@ -8,10 +7,6 @@ const routes = [
   {
     path: "/",
     component: HomeView,
-  },
-  {
-    path: "/about",
-    component: AboutView,
   },
   {
     path: "/stadiums/add",
